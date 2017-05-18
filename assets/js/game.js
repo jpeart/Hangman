3e91965e1fb8f2ed452718fa13e4aa0872120026
jpeart <jpeart4@gmail.com>
2017-05-17 20:24:00 -0500

    final 2p added

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,14 @@ var startpic = "assets/images/start.png";
 var nickCage = "assets/images/end.png";
 var png = ".png";
 
+var input = '';
 
 var guesses = 11;
 var guesscounter = 11;
-var pic = 0;
 var piccounter = 0;
 
-var play = true;
-var done = false;
+var play = false;
+var win = false;
 var first = true;
 
 var ammend = '';
@@ -22,29 +22,41 @@ var answer = "ayywhatup";
 var showme = '';
 var absent = -1;
 
-//make answer into array of chars to check using indexof
-checkme = answer.split('');
-//Create the blank spaces to show on page
-for (var i = 0; i < checkme.length; i++) {
-    showme += "_ ";
-}
-showme = showme.toString();
-document.getElementById("answer").innerHTML = showme;
-showme = showme.split('');
-
-//initialize count
-document.getElementById("count").innerHTML = guesscounter;
-
 document.onkeyup = function(event) {
-        // Determine which key was pressed
-        var userGuess = event.key;
-        // check if alphabet
-        //alert("you pressed " + userGuess);
+        // PLAYER 1 SETUP
+        if (!play) {
+            //validate input text
+            document.getElementById("word").onkeyup = function(event) {
+                // Determine which key was pressed
+                var userGuess = event.key;
+                // check if alphabet
+                //alert("you pressed " + userGuess);
+                if (userGuess.search(/^[a-zA-Z]+$/) === absent) {
+                    alert("You know, I trusted you, but you had to go and input something that isn't a letter");
+                    input = document.getElementById("word").value;
+                    input = input.split('');
 
-        if (userGuess.search(/^[a-zA-Z]+$/) === absent || nonalpha.indexOf(userGuess) != absent) {
-            alert("Use the alphabet dick");
-        } else {
-            if (play) {
+                    // remove the dirty non-alphabet character
+                    // can search through input to find if multiple keys were pressed at same time
+                    // to remove the faulty character but too lazy 
+                    if (input.length > 0) {
+                        input.length--;
+                        document.getElementById("word").value = input.join('');
+                    }
+
+                }
+            }
+        } //end player1 if
+        //HANGMAN GAME CODE
+        else {
+            // Determine which key was pressed
+            var userGuess = event.key;
+            // check if alphabet
+            //alert("you pressed " + userGuess);
+
+            if (userGuess.search(/^[a-zA-Z]+$/) === absent || nonalpha.indexOf(userGuess) != absent) {
+                alert("Use the alphabet dick");
+            } else {
                 //check if has been used before (true if has been)
                 if (used.indexOf(userGuess) != absent) {
                     alert("You already guessed that bro");
@@ -60,10 +72,7 @@ document.onkeyup = function(event) {
                             if (checkme[i] == userGuess)
                                 showme[i * 2] = userGuess;
                         } //end for
-                        showme = showme.join('');
-                        showme = showme.toString();
-                        document.getElementById("answer").innerHTML = showme;
-                        showme = showme.split('');
+                        showcorrect();
 
                         //check if done
                         //this shallow copy is giving me trouble, havent used 'new' in javascript dont know how to make it not a reference
@@ -82,16 +91,9 @@ document.onkeyup = function(event) {
                             // array[0] = a array[1] = ' ' when spliced array[0] = a array[1] = y 
                         }
                         // done
-                        // console.log(copy);
-                        // console.log(answer);
-
                         if (copy.join('') == answer) {
-                            alert("You Win!");
-                            if (confirm("Play Again?")) {
-                                reset();
-                            } else {
-                                window.close();
-                            }
+                            win = true;
+                            end(win);
                         }
                     }
                     // wrong guess
@@ -100,13 +102,8 @@ document.onkeyup = function(event) {
                         guesscounter--;
                         // last guess
                         if (piccounter > guesses) {
-                            document.getElementById("pic").src = nickCage;
-                            alert("You Lose!");
-                            if (confirm("Play Again?")) {
-                                reset();
-                            } else {
-                                window.close();
-                            }
+                            win = false;
+                            end(win);
                         } else {
                             //change guess count
                             document.getElementById("count").innerHTML = guesscounter;
@@ -114,50 +111,92 @@ document.onkeyup = function(event) {
                             used.push(userGuess);
                             //print guess to screen
                             ammend = document.getElementById("guesses").innerHTML;
+                            //dont want no commas before the first letter of the list
                             if (first) {
                                 ammend += userGuess;
                                 document.getElementById("guesses").innerHTML = ammend
                                 first = false;
-
                             } else {
                                 ammend += ", " + userGuess;
                                 document.getElementById("guesses").innerHTML = ammend;
 
                             }
                             // change picture
-                            path = "assets/images/";
-                            pic = piccounter;
-                            pic.toString();
-                            pic += png;
-                            path += pic;
-                            document.getElementById("pic").src = path;
+                            changepix(piccounter);
                         } // end change picture
                     } // end wrong guess else
                 } // end else if used
-            } // end if play
+            } // end is alphabet else
+        } // end keystroke function
+    } //end if play
 
 
-        } // end is alphabet else
+// le functions
+//display correct guess on screen
+function showcorrect() {
+    showme = showme.join('');
+    showme = showme.toString();
+    document.getElementById("answer").innerHTML = showme;
+    showme = showme.split('');
+}//end showcorrect()
 
+// Called on button press Switch from player 1 to player 2
+// pull word from textbox add it to "answer"
+function begin() {
+    input = document.getElementById("word").value;
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("p2").style.display = "block";
+    answer = input;
+    play = true;
+    initialize(checkme, answer, showme, guesscounter);
+}//end begin()
 
-    } // end keystroke function
-
-function reset() {
-    document.getElementById("pic").src = "assets/images/start.png"
-    piccounter = 0;
-    used = [];
-    showme = [];
-    guesscounter = 0;
+// function puts answers blanks and guess counter to screen
+// thought arguments were naturally reference not value in javascript, guess not.
+function initialize(c, a, s, g) {
+    //make answer into array of chars to check using indexof
+    checkme = answer.split('');
+    //Create the blank spaces to show on page
     for (var i = 0; i < checkme.length; i++) {
         showme += "_ ";
     }
     showme = showme.toString();
     document.getElementById("answer").innerHTML = showme;
     showme = showme.split('');
+    //initialize count
+    document.getElementById("count").innerHTML = guesscounter;
+}//end initialize()
 
+function changepix(x) {
+    var pic = 0;
+    path = "assets/images/";
+    pic = x;
+    pic.toString();
+    pic += png;
+    path += pic;
+    document.getElementById("pic").src = path;
+}//end changepix()
+
+// self explainatory
+function reset() {
+    play = false;
+    document.getElementById("pic").src = "assets/images/start.png"
+    piccounter = 0;
+    used = [];
+    showme = [];
+    guesscounter = 11;
+    guesses = 11;
+    first = true;
+    // for (var i = 0; i < checkme.length; i++) {
+    //     showme += "_ ";
+    // }
+    // showme = showme.toString();
+    // document.getElementById("answer").innerHTML = showme;
+    // showme = showme.split('');
     document.getElementById("guesses").innerHTML = "Incorrect Guesses : ";
-
-}
+    document.getElementById("p1").style.display = "block";
+    document.getElementById("p2").style.display = "none";
+}// end reset()
 
 //make a deep copy of array 'x' and return it
 function copyarray(x) {
@@ -169,3 +208,20 @@ function copyarray(x) {
     }
     return newcopy;
 } //end copyarray
+
+function end(b) {
+    if (b) {
+        alert("You Win!");
+        if (confirm("Play Again?"))
+            reset();
+        else
+            window.close();
+    } else {
+        document.getElementById("pic").src = nickCage;
+        alert("You Lose!");
+        if (confirm("Play Again?"))
+            reset();
+        else
+            window.close();
+    } //endelse
+}//end end()
